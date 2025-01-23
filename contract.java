66987101f3ce8965096345e54f5d7d2e3e2ef5cf
Illiath <s-selpht@bsd405.org>
2025-01-23 09:54:54 -0800

    Working on it.
    Things are broken right now.
    Storyboarding done in collaboration with Ed.
    
    Co-Authored-By: EduardoGBlanco <122505795+EduardoGBlanco@users.noreply.github.com>

--- contract.java
+++ contract.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class contract {
-  public static void Main (String[] args) {
+  public static void Main (String[] args) throws InterruptedException {
+
+    Scanner scan = new Scanner(System.in);
+    String name;
 
     // Assignment 1.1: Loyalty Test
 
@@ -12,7 +17,37 @@ public class contract {
     
     // Code below is HIDDEN
 
+    System.out.printf("Welcome to the Nacreous Intelligent Systems Hiring Center.");
+    for (int i = 0; i < 5; i++) {
+      Thread.sleep(50);
+      System.out.printf("\n");
+    }
+
+    System.out.printf("Do you accept the license agreement? (y/n) ");
+    String acc = scan.next();
+    System.out.printf("\n");
+    Thread.sleep(500);
+
+    System.out.printf("Processing");
+      for (int i = 0; i < 3; i++) {
+        Thread.sleep(500);
+        System.out.printf(".");
+      }
+      Thread.sleep(1000);
+      System.out.printf("\n");
+
+
+    if (eula == false && isTemp == false) {
+      System.out.printf("Error: License not accepted. Please open contract.java to accept the license agreement, then restart the program.");
+    }
+    else if (eula == true && isTemp == true) {
+      System.out.printf("Error: License mismatch. User has agreed to one (1) or more conflicting licenses.");
+    }
+    else if (eula == true && !acc.toLowerCase().equals("y")) {
+      System.out.printf("")
+    }
     
+
 
   }
 }
